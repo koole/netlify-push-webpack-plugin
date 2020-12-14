2ba32ae7da03d4ec17cd53616c1cdfa10fcbc98b
Leon Koole <leon@koole.io>
2020-12-14 22:27:43 +0100

    Remove hmr option from mini-css-extract-plugin

--- tests/all/webpack.config.js
+++ tests/all/webpack.config.js
@@ -17,10 +17,7 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              // you can specify a publicPath here
-              // by default it uses publicPath in webpackOptions.output
               publicPath: "../",
-              hmr: process.env.NODE_ENV === "development"
             }
           },
           "css-loader"
